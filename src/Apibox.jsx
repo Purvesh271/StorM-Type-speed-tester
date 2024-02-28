e65eaf1bae87bdb679c4868b773dc28f77165d60
Purvesh271 <ppurvesh923@gmail.com>
2024-02-29 00:20:05 +0530

    made typing speed and typing box functions

--- src/Apibox.jsx
+++ src/Apibox.jsx
@@ -3,7 +3,8 @@ import './Apibox.css'
 
 export default function Apibox(){
 
-    let[Text, setText] = useState({});
+    let[Text, setText] = useState('');
+    let [showResult, setShowResult] = useState(false);
 
     const url = "https://api.quotable.io/random?minLength=100&maxLength=150";
 
@@ -25,15 +26,34 @@ export default function Apibox(){
     }, []);
 
     //Timer 
-    let [Time,setTime] = useState(30);
+    let [Time,setTime] = useState(10);
     let reducedTime = ()=>{
         const timer = setInterval(() => {
             if(Time >= 0) {
                 setTime(Time => Time - 1)
             } else {
                 clearInterval(timer);
+                calculateTypingSpeed();
             }
         }, 1000);
+    };
+
+    // Handle typing
+    let [typeTxt,settypeTxt] = useState('');
+    let handleTyping = (event) => {
+        let { value } = event.target;
+        settypeTxt(value);
+    };
+
+    // Calculate typing speed
+    let [typingSpeed, setTypingSpeed] = useState(null);
+
+    const calculateTypingSpeed = () => {
+        const timeTakenInSeconds = 10;
+        const wordCount = typeTxt.trim().split(/\s+/).length;
+        const typingSpeed = Math.round((wordCount / timeTakenInSeconds) * 60);
+        setTypingSpeed(typingSpeed);
+        setShowResult(true);
     };
 
 
@@ -50,6 +70,18 @@ export default function Apibox(){
             <button onClick={reducedTime}>Start</button>
             <h3>Time: {Time >= 0 ? Time : 0} </h3>
         </div>
+
+        {Time === 0 && (
+            <div className="result">
+                <h2>Typing Speed Result:</h2>
+                <p>Your typing speed is: {typingSpeed} WPM</p>
+            </div>
+        )}
+
+
+
+        <h4  contentEditable = "true" className='userBox' onChange={handleTyping} value={}> </h4 >
+
 
     </div>
     );
--- src/Typebox.jsx
+++ src/Typebox.jsx
@@ -1,11 +1,13 @@
 import './Typebox.css'
+import Apibox from './Apibox';
 
 
 export default function Typebox(){
 
     return(
         <div>
-            <h4  contentEditable = "true" className='userBox'> </h4 >
+            <h4  contentEditable = "true" className='userBox' onChange={handleTyping} value={text}> </h4 >
+            
         </div>
     );
 }
